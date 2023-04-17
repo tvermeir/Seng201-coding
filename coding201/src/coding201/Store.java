0632533389809a56604990ef21c9c5646291957b
toddv <toddv@Todds_Computer>
2023-04-17 15:03:54 +1200

    Upload

--- coding201/src/coding201/Store.java
+++ coding201/src/coding201/Store.java
@@ -68,7 +68,7 @@ public class Store {
 			System.out.println("Please enter in a correct name");
 			doesContain(playerName, athleteName);
 		}
-		return "ffsds";
+		return "ffss";
 		
 	}
 	public static void main(String[] args) {
