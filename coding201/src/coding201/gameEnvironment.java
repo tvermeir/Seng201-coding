e44b65507262576a493d678d17bbcbc70533c1bf
toddv <toddv@Todds_Computer>
2023-04-19 21:24:01 +1200

    gdfgf

--- coding201/src/coding201/gameEnvironment.java
+++ coding201/src/coding201/gameEnvironment.java
@@ -1,5 +1,4 @@
 package coding201;
-import java.util.ArrayList;
 import java.util.Hashtable;
 
 public class gameEnvironment {
@@ -7,51 +6,38 @@ public class gameEnvironment {
 	double difficulty;
 	int money = 198;
 	
-	Hashtable<String, String> playerHashTable = new Hashtable<>();
 	Hashtable<String, String> athletesOnTeam = new Hashtable<>();
-	ArrayList<Athlete> purchasableAthletes = new ArrayList<Athlete>();
-	ArrayList<Athlete> athletesinClub = new ArrayList<Athlete>();
+	Hashtable<String, String> reserveListHashtable = new Hashtable<>();
 	
 	
 	
 	
 	public void getDetails(clubSetup myClub) {
-		clubName = myClub.clubName();
-		difficulty = myClub.setDifficulty();
-		money = myClub.getMoney();
+//		clubName = myClub.clubName();
+//		difficulty = myClub.setDifficulty();
+//		money = myClub.getMoney();
 		athletesOnTeam = myClub.getPlayers();
 		
 	}
-	public Hashtable<String, String> addPurchasablePlayers(PionelPessi pepsi, PistaPenaldo penaldo, GuiltySiggurdson child, Darwizzey darwin) {
-		playerHashTable.put(pepsi.getName(), pepsi.getHashtable());
-		playerHashTable.put(penaldo.getName(), penaldo.getHashtable());
-		playerHashTable.put(child.getName(), child.getHashtable());
-		playerHashTable.put(darwin.getName(), darwin.getHashtable());
+	
+	public Store storeMethod() {
+		Store store = new Store(money, athletesOnTeam);
+		return store;
 		
-
-		return playerHashTable;
 	}
-	public void storeMethod() {
-		Store store = new Store(playerHashTable, money, athletesOnTeam);
-		money = store.purchaseAthlete();
-		System.out.println(money);
-	}
+	
 	
 	
 	public static void main(String[] args) {
 		
-		
+		//Club Setup
 		gameEnvironment helloEnvironment = new gameEnvironment();
 		clubSetup myClub = new clubSetup();
 		helloEnvironment.getDetails(myClub);
 		
 		
 		
-		AthleteDatabase ting = new AthleteDatabase();
-		Athlete nignog = ting.getAthlete(myClub.difficultyMultiplier);
-		System.out.println(nignog);
 		
-<<<<<<< main
 		//Store Method 
 		Store store = helloEnvironment.storeMethod();
 		store.refreshStore();
@@ -61,19 +47,6 @@ public class gameEnvironment {
 		System.out.println(helloEnvironment.money);
 		helloEnvironment.money = store.purchaseAthlete();
 		System.out.println(helloEnvironment.money);
-=======
-		
-//		
-		
-		
-		
-//		PionelPessi pepsi = new PionelPessi();
-//		PistaPenaldo penaldo = new PistaPenaldo();
-//		GuiltySiggurdson child = new GuiltySiggurdson(); 
-//		Darwizzey darwin = new Darwizzey();
-//		helloEnvironment.addPurchasablePlayers(pepsi, penaldo, child, darwin);
-//		helloEnvironment.storeMethod();
->>>>>>> 17de8e8 Commitment
 		
 		
 	}
