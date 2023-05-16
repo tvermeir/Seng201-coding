fde2df82d00b229543b46412681d5ed79acb77f6
benbe <benbe@benpc>
2023-05-16 15:26:05 +1200

    oops fixed

--- coding201/src/coding201/clubSetupFrame.java
+++ coding201/src/coding201/clubSetupFrame.java
@@ -12,8 +12,6 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JRadioButton;
 import java.awt.Color;
-import javax.swing.JScrollBar;
-import javax.swing.JSlider;
 
 public class clubSetupFrame extends JPanel {
 
@@ -33,18 +31,11 @@ public class clubSetupFrame extends JPanel {
 		 JPanel clubPanel = new JPanel();
 		 add(clubPanel);
 		 
-		 JLabel titleLabel = new JLabel("Select the Number of Weeks You Would Like to Play For");
+		 JLabel titleLabel = new JLabel("Select a Difficulty");
 		 titleLabel .setFont(new Font("Bahnschrift", Font.PLAIN, 33));
 		 titleLabel .setHorizontalAlignment(SwingConstants.CENTER);
 		 titleLabel .setBounds(0, 0, 1280, 99);
 		 add(titleLabel);
-		 
-		 JSlider framesPerSecond = new JSlider();
-		 framesPerSecond.setPaintLabels(true);
-		 framesPerSecond.setPaintTicks(true);
-		 framesPerSecond.setMinorTickSpacing(2);
-		 framesPerSecond.setBounds(269, 347, 741, 45);
-		 add(framesPerSecond);
 		 
 		 getTextFromTextField();
 		 
@@ -53,9 +44,32 @@ public class clubSetupFrame extends JPanel {
 	}
 	public String getTextFromTextField() {
 		 
-		
+		 JRadioButton easyButton = new JRadioButton("Easy");
+		 easyButton.setFont(new Font("Lucida Grande", Font.PLAIN, 18));
+		 easyButton.setBounds(569, 197, 141, 23);
+		 add(easyButton);
+		 
+		 JRadioButton mediumButton = new JRadioButton("Medium");
+		 mediumButton.setFont(new Font("Lucida Grande", Font.PLAIN, 18));
+		 mediumButton.setBounds(569, 263, 141, 23);
+		 add(mediumButton);
+		 
+		 JRadioButton hardButton = new JRadioButton("Hard");
+		 hardButton.setFont(new Font("Lucida Grande", Font.PLAIN, 18));
+		 hardButton.setBounds(569, 344, 141, 23);
+		 add(hardButton);
+		 
+		 JButton confirmDifficultyButton = new JButton("Confirm Difficulty");
+		 confirmDifficultyButton.addActionListener(new ActionListener() {
+		 	public void actionPerformed(ActionEvent e) {
+		 	}
+		 });
+		 confirmDifficultyButton.setBackground(Color.WHITE);
+		 confirmDifficultyButton.setFont(new Font("Lucida Grande", Font.PLAIN, 18));
+		 confirmDifficultyButton.setBounds(508, 448, 264, 29);
+		 add(confirmDifficultyButton);
 		 
 		 
-		 
+		 return clubName;
 	}
 }
